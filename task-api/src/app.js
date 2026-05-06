0a879034513e49a58ed8e77fb880edcdd380f532
Sudip Kumar Prasadmkdir <sudipkumarprasad2005@gmail.com>
2026-05-06 19:55:19 +0530

    feat: enable static file serving for frontend

--- task-api/src/app.js
+++ task-api/src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const path = require('path');
 const taskService = require('./taskService');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/tasks', (req, res) => {
   const { status, page, limit } = req.query;
